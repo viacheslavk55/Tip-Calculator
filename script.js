765c49e2369c2c99623c74ec37e619a8e3b7ac83
viacheslavk55 <525vkostenko@frhsd.com>
2024-01-22 09:30:54 -0500

    did the function

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let bill = 120
+function calculateAndDisplay() {
+    //Get the input values from our HTML forms
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    let numOfPpl = parseInt(document.getElementById('numOfPeople').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
+
+
+    //Calculate tip, total bill, and amount per person using helper functions
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill(bill, tip)
+    let amtPerPers = calculateAmtPerPers(totalBill, numOfPpl)
+
+    //Display the results in the HTML document
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText = 'Total Bill: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText = 'Amount per person: $' + amtPerPers.toFixed(2)
+}
+
+
+
+
+
+
+
+
+
+
+/* let bill = 120
 let numOfPpl = 5
 let serviceQuality = 'Great'
 
@@ -30,4 +57,4 @@ let amtPerPers = calcAmtPerPrs(totalBill, numOfPpl)
 
 console.log('Tip: $' + tip)
 console.log('Total Bill: $' + totalBill)
-console.log('Amount per person: $' + amtPerPers)
+console.log('Amount per person: $' + amtPerPers) */
